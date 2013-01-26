7699accd4d5844bd7890b73c26eab99d4a7071fd
Graham Hinchly <grahamhinchly@Grahams-MacBook-Air.local>
2013-01-26 17:36:58 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordWar::Application.config.secret_token = '00af688a0dddc5ad5a2eba98b448e61fda925fa2eda0a29306bd3aee12ed175ac132f241da160bd6537f7fd65e71062b72c169c40ccc6269f6328f9d3bd7db51'
