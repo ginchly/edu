53debeb72bbeffe536d8d674a6d81f82c2777dfc
Graham Hinchly <grahamhinchly@Grahams-MacBook-Air.local>
2013-01-27 08:37:06 +0000

    added heroku keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret']
+  #dev
+  #provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret']
+  #heroku
+  provider :facebook, "410916555661149", "ee1d4551ae60a4ef3c4a910581229811"
+  
+  	
 end
